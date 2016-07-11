eefa0101598e3a8a3f668104746047d19a8d2caf
lixin <lixin@lsh123.com>
2016-07-11 21:39:17 +0800

    增加update 、insert

--- wms-core/src/main/java/com/lsh/wms/core/service/csi/CsiSupplierService.java
+++ wms-core/src/main/java/com/lsh/wms/core/service/csi/CsiSupplierService.java
@@ -3,6 +3,7 @@ package com.lsh.wms.core.service.csi;
 import com.lsh.wms.core.dao.csi.CsiOwnerDao;
 import com.lsh.wms.core.dao.csi.CsiSupplierDao;
 import com.lsh.wms.model.csi.CsiOwner;
+import com.lsh.wms.model.csi.CsiSku;
 import com.lsh.wms.model.csi.CsiSupplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,11 +25,12 @@ import java.util.concurrent.ConcurrentMap;
 @Transactional(readOnly = true)
 public class CsiSupplierService {
     private static final Logger logger = LoggerFactory.getLogger(CsiSupplierService.class);
-    private static final ConcurrentMap<Integer, CsiSupplier> m_SupplierCache = new ConcurrentHashMap<Integer, CsiSupplier>();
+    //将int改为long modify by lixin
+    private static final ConcurrentMap<Long, CsiSupplier> m_SupplierCache = new ConcurrentHashMap<Long, CsiSupplier>();
     @Autowired
     private CsiSupplierDao supplierDao;
-
-    public CsiSupplier getSupplier(int iSupplierId){
+    //将int改为long modify by lixin
+    public CsiSupplier getSupplier(long iSupplierId){
         CsiSupplier cat = m_SupplierCache.get(iSupplierId);
         if(cat == null){
             Map<String, Object> mapQuery = new HashMap<String, Object>();
@@ -42,6 +44,42 @@ public class CsiSupplierService {
             }
         }
         return cat;
+    }
+
+    @Transactional(readOnly = false)
+    public CsiSupplier insertSupplier(CsiSupplier supplier){
+        if(supplier.getSupplierId()==0){
+            {
+                //gen supplier_id
+                int iSupplierId = 0;
+                int count = supplierDao.countCsiSupplier(null);
+                if(count==0){
+                    iSupplierId = 1;
+                }else{
+                    iSupplierId = 1 + count;
+                }
+                supplier.setSupplierId((long)iSupplierId);
+            }
+        }
+        supplierDao.insert(supplier);
+        return supplier;
+    }
+
+    @Transactional(readOnly = false)
+    public int updateSupplier(CsiSupplier supplier){
+        //判断是否存在
+        if(this.getSupplier(supplier.getSupplierId()) == null){
+            return -1;
+        }
+        //更新供应商信息
+        supplierDao.update(supplier);
+
+        //更新缓存中的数据
+        Map<String, Object> mapQuery = new HashMap<String, Object>();
+        mapQuery.put("supplierId", supplier.getSupplierId());
+        CsiSupplier newSupplier = supplierDao.getCsiSupplierList(mapQuery).get(0);
+        m_SupplierCache.put(supplier.getSupplierId(),newSupplier);
+        return 0;
     }
 
 }
