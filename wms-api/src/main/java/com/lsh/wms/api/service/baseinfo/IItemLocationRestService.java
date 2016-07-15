662ac6644b1a64aa3199b5cd31b1048fd785c905
lixin <lixin@lsh123.com>
2016-07-15 11:04:32 +0800

    增加item跟拣货位关系表

---
+++ wms-api/src/main/java/com/lsh/wms/api/service/baseinfo/IItemLocationRestService.java
@@ -0,0 +1,18 @@
+package com.lsh.wms.api.service.baseinfo;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.lsh.wms.model.baseinfo.BaseinfoItemLocation;
+
+import java.util.List;
+
+/**
+ * Created by lixin-mac on 16/7/14.
+ */
+@Service(protocol = "rest")
+public interface IItemLocationRestService {
+
+    String getItemLocation(long iSkuId,long iOwnerId);
+    String getItemLocationByLocationID(long iLocationId);
+    String insertItemLocation(BaseinfoItemLocation itemLocation);
+    String updateItemLocation(BaseinfoItemLocation itemLocation);
+}
