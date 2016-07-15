f87c86d9242ee9a143e8d1f6974969cd8b4bbd8d
lixin <lixin@lsh123.com>
2016-07-15 11:05:03 +0800

    增加item跟拣货位关系表

---
+++ wms-core/src/main/java/com/lsh/wms/core/dao/baseinfo/BaseinfoItemLocationDao.java
@@ -0,0 +1,22 @@
+package com.lsh.wms.core.dao.baseinfo;
+
+import com.lsh.wms.core.dao.MyBatisRepository;
+import com.lsh.wms.model.baseinfo.BaseinfoItemLocation;
+
+import java.util.List;
+import java.util.Map;
+
+@MyBatisRepository
+public interface BaseinfoItemLocationDao {
+
+	void insert(BaseinfoItemLocation baseinfoItemLocation);
+	
+	void update(BaseinfoItemLocation baseinfoItemLocation);
+	
+	BaseinfoItemLocation getBaseinfoItemLocationById(Long id);
+
+    Integer countBaseinfoItemLocation(Map<String, Object> params);
+
+    List<BaseinfoItemLocation> getBaseinfoItemLocationList(Map<String, Object> params);
+	
+}
