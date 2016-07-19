b2a0e21f74f4ae54ce5993adcfc2d7a3aea5fd94
luckMing <heming@lsh123.com>
2016-07-19 16:51:54 +0800

    add comment

--- wms-model/src/main/java/com/lsh/wms/model/stock/StockLot.java
+++ wms-model/src/main/java/com/lsh/wms/model/stock/StockLot.java
@@ -31,75 +31,75 @@ public class StockLot implements Serializable {
 
 	private Long receiptId;
 
-	
+
 	public Long getId(){
 		return this.id;
 	}
-	
+
 	public void setId(Long id){
 		this.id = id;
 	}
-	
+
 	public Long getLotId(){
 		return this.lotId;
 	}
-	
+
 	public void setLotId(Long lotId){
 		this.lotId = lotId;
 	}
-	
+
 	public Long getSkuId(){
 		return this.skuId;
 	}
-	
+
 	public void setSkuId(Long skuId){
 		this.skuId = skuId;
 	}
-	
+
 	public String getSerialNo(){
 		return this.serialNo;
 	}
-	
+
 	public void setSerialNo(String serialNo){
 		this.serialNo = serialNo;
 	}
-	
+
 	public Long getInDate(){
 		return this.inDate;
 	}
-	
+
 	public void setInDate(Long inDate){
 		this.inDate = inDate;
 	}
-	
+
 	public Long getProductDate(){
 		return this.productDate;
 	}
-	
+
 	public void setProductDate(Long productDate){
 		this.productDate = productDate;
 	}
-	
+
 	public Long getExpireDate(){
 		return this.expireDate;
 	}
-	
+
 	public void setExpireDate(Long expireDate){
 		this.expireDate = expireDate;
 	}
-	
+
 	public Long getCreatedAt(){
 		return this.createdAt;
 	}
-	
+
 	public void setCreatedAt(Long createdAt){
 		this.createdAt = createdAt;
 	}
-	
+
 	public Long getUpdatedAt(){
 		return this.updatedAt;
 	}
-	
+
 	public void setUpdatedAt(Long updatedAt){
 		this.updatedAt = updatedAt;
 	}
