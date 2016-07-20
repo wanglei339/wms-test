d2753b54e2cc121953ff79a2d053953b62d45a3a
Panxudong <18686095392@163.com>
2016-07-20 15:47:21 +0800

    OutbDeliveryHeader,OutbDeliveryDetail

--- wms-core/src/main/java/com/lsh/wms/core/service/po/PoReceiptService.java
+++ wms-core/src/main/java/com/lsh/wms/core/service/po/PoReceiptService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Project Name: lsh-wms
@@ -30,7 +31,11 @@ public class PoReceiptService {
     @Autowired
     private InbReceiptHeaderDao inbReceiptHeaderDao;
 
-
+    /**
+     * 插入InbReceiptHeader及List<InbReceiptDetail>
+     * @param inbReceiptHeader
+     * @param inbReceiptDetailList
+     */
     @Transactional(readOnly = false)
     public void orderInit(InbReceiptHeader inbReceiptHeader, List<InbReceiptDetail> inbReceiptDetailList){
         inbReceiptHeader.setInserttime(new Date());
@@ -41,11 +46,70 @@ public class PoReceiptService {
         inbReceiptDetailDao.batchInsert(inbReceiptDetailList);
     }
 
+    /**
+     * 插入InbReceiptHeader及List<InbReceiptDetail>
+     * @param inbReceiptHeader
+     * @param inbReceiptDetailList
+     */
     @Transactional(readOnly = false)
     public void insertOrder(InbReceiptHeader inbReceiptHeader, List<InbReceiptDetail> inbReceiptDetailList) {
         //插入订单
         inbReceiptHeader.setInserttime(new Date());
         inbReceiptHeaderDao.insert(inbReceiptHeader);
         inbReceiptDetailDao.batchInsert(inbReceiptDetailList);
+    }
+
+    /**
+     * 通过ID获取InbReceiptHeader
+     * @param id
+     * @return
+     */
+    public InbReceiptHeader getInbReceiptHeaderById(Long id) {
+        return inbReceiptHeaderDao.getInbReceiptHeaderById(id);
+    }
+
+    /**
+     * 根据参数获取InbReceiptHeader数量
+     * @param params
+     * @return
+     */
+    public Integer countInbReceiptHeader(Map<String, Object> params) {
+        return inbReceiptHeaderDao.countInbReceiptHeader(params);
+    }
+
+    /**
+     * 自定义参数获取List<InbReceiptHeader>
+     * @param params
+     * @return
+     */
+    public List<InbReceiptHeader> getInbReceiptHeaderList(Map<String, Object> params) {
+        return inbReceiptHeaderDao.getInbReceiptHeaderList(params);
+    }
+
+    /**
+     * 通过ID获取InbReceiptDetail
+     * @param id
+     * @return
+     */
+    public InbReceiptDetail getInbReceiptDetailById(Long id) {
+        return inbReceiptDetailDao.getInbReceiptDetailById(id);
+    }
+
+    /**
+     * 根据参数获取InbReceiptDetail数量
+     * @param params
+     * @return
+     */
+    public Integer countInbReceiptDetail(Map<String, Object> params) {
+        return inbReceiptDetailDao.countInbReceiptDetail(params);
+    }
+
+    /**
+     * 自定义参数获取List<InbReceiptDetail>
+     * @param params
+     * @return
+     */
+    public List<InbReceiptDetail> getInbReceiptDetailList(Map<String, Object> params) {
+        return inbReceiptDetailDao.getInbReceiptDetailList(params);
     }
 }
