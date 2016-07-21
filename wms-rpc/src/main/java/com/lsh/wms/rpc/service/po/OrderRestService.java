1b1d73a67d8845d87a6443850c9154e4e0bdca30
fuhao <fuhao@lsh123.com>
2016-07-21 14:34:46 +0800

    po订单

---
+++ wms-rpc/src/main/java/com/lsh/wms/rpc/service/po/OrderRestService.java
@@ -0,0 +1,28 @@
+package com.lsh.wms.rpc.service.po;
+
+import com.lsh.wms.api.service.po.IOrderRestService;
+
+import java.util.Map;
+
+/**
+ * Project Name: lsh-wms
+ * Created by fuhao
+ * Date: 16/7/21
+ * Time: 16/7/21.
+ * 北京链商电子商务有限公司
+ * Package name:com.lsh.wms.rpc.service.po.
+ * desc:类功能描述
+ */
+public class OrderRestService implements IOrderRestService{
+    public String getPoDetailByOrderId(Long orderId) {
+        return null;
+    }
+
+    public Integer countInbPoHeader(Map<String, Object> params) {
+        return null;
+    }
+
+    public String getPoDetailList(Map<String, Object> params) {
+        return null;
+    }
+}
