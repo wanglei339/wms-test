a18388b7c92da8469ca8fed357e15ab07d032f37
wuhao4830 <425677852@qq.com>
2016-07-23 15:04:46 +0800

    fix

--- wms-rpc/src/main/java/com/lsh/wms/rpc/service/inhouse/StockTakingRpcService.java
+++ wms-rpc/src/main/java/com/lsh/wms/rpc/service/inhouse/StockTakingRpcService.java
@@ -11,10 +11,10 @@ import javax.ws.rs.core.MediaType;
 /**
  * Created by mali on 16/7/22.
  */
-@Service(protocol = "rest")
-@Path("inhouse")
-@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
-@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
+//@Service(protocol = "rest")
+//@Path("inhouse")
+//@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
+//@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
 public class StockTakingRpcService {
 
 }
