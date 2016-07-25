755de3f0d0bbe8d265d1e3fbe44be5be3a784cb1
zengwenjun <zengwenjun@lsh123.com>
2016-07-25 20:37:48 +0800

    add task info sub type

---
+++ wms-core/src/main/java/com/lsh/wms/core/constant/PickConstant.java
@@ -0,0 +1,9 @@
+package com.lsh.wms.core.constant;
+
+/**
+ * Created by zengwenjun on 16/7/25.
+ */
+public class PickConstant {
+    public static final Long SHELF_TASK_TYPE = 1L;
+    public static final Long LOFT_TASK_TYPE = 2L;
+}
