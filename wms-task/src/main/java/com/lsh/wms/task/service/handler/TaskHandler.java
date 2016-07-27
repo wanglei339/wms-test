99ffffeb9cb0297f0e0b2a805fa2c26f3af944b0
Frank.F <fancal_king@163.com>
2016-07-27 10:19:35 +0800

    update shelve

--- wms-task/src/main/java/com/lsh/wms/task/service/handler/TaskHandler.java
+++ wms-task/src/main/java/com/lsh/wms/task/service/handler/TaskHandler.java
@@ -1,5 +1,6 @@
 package com.lsh.wms.task.service.handler;
 
+import com.lsh.base.common.exception.BizCheckedException;
 import com.lsh.wms.model.task.TaskEntry;
 import com.lsh.wms.model.task.TaskInfo;
 
@@ -10,8 +11,8 @@ import java.util.Map;
  * Created by mali on 16/7/23.
  */
 public interface TaskHandler {
-    void create(TaskEntry taskEntry);
-    void batchCreate(List<TaskEntry> taskEntries);
+    void create(TaskEntry taskEntry) throws BizCheckedException;
+    void batchCreate(List<TaskEntry> taskEntries) throws BizCheckedException;
     TaskEntry getTask(Long taskId);
     List<TaskEntry> getTaskList(Map<String, Object> condition);
     int getTaskCount(Map<String, Object> condition);
@@ -20,5 +21,6 @@ public interface TaskHandler {
     void allocate(Long taskId);
     void release(Long taskId);
     void done(Long taskId);
+    void done(Long taskId, Long locationId) throws BizCheckedException;
     void cancel(Long taskId);
 }
--- wms-task/src/main/java/com/lsh/wms/task/service/task/shelve/ShelveTaskHandler.java
+++ wms-task/src/main/java/com/lsh/wms/task/service/task/shelve/ShelveTaskHandler.java
@@ -1,11 +1,18 @@
 package com.lsh.wms.task.service.task.shelve;
 
+import com.lsh.base.common.exception.BizCheckedException;
+import com.lsh.base.common.utils.DateUtils;
 import com.lsh.wms.core.constant.TaskConstant;
+import com.lsh.wms.core.service.container.ContainerService;
+import com.lsh.wms.core.service.location.LocationService;
+import com.lsh.wms.model.baseinfo.BaseinfoContainer;
+import com.lsh.wms.model.baseinfo.BaseinfoLocation;
 import com.lsh.wms.model.shelve.ShelveTaskHead;
 import com.lsh.wms.model.task.TaskEntry;
 import com.lsh.wms.task.service.handler.AbsTaskHandler;
 import com.lsh.wms.task.service.handler.TaskHandlerFactory;
 import com.lsh.wms.core.service.shelve.ShelveTaskService;
+import com.lsh.wms.rpc.service.shelve.ShelveRpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,16 +27,52 @@ public class ShelveTaskHandler extends AbsTaskHandler {
     private TaskHandlerFactory handlerFactory;
     @Autowired
     private ShelveTaskService taskService;
+    @Autowired
+    private ContainerService containerService;
+    @Autowired
+    private ShelveRpcService shelveRpcService;
+    @Autowired
+    private LocationService locationService;
 
     @PostConstruct
     public void postConstruct() {
         handlerFactory.register(TaskConstant.TYPE_SHELVE, this);
     }
 
-    protected void createConcrete(TaskEntry taskEntry) {
+    protected void createConcrete(TaskEntry taskEntry) throws BizCheckedException {
         ShelveTaskHead taskHead = (ShelveTaskHead) taskEntry.getTaskHead();
         taskHead.setTaskId(taskEntry.getTaskInfo().getTaskId());
+        Long containerId = taskHead.getContainerId();
+        BaseinfoContainer container = containerService.getContainer(containerId);
+        if (container == null) {
+            throw new BizCheckedException("2030003");
+        }
+        // 获取目标location
+        BaseinfoLocation targetLocation = shelveRpcService.assginShelveLocation(container);
+        if (targetLocation == null) {
+            throw new BizCheckedException("2030004");
+        }
+        taskHead.setAllocLocationId(targetLocation.getLocationId());
         taskService.create(taskHead);
+    }
+
+    protected void assignConcrete(Long taskId, Long staffId) {
+        taskService.assign(taskId, staffId);
+    }
+
+    protected void doneConcrete(Long taskId, Long locationId) throws BizCheckedException{
+        ShelveTaskHead taskHead = taskService.getShelveTaskHead(taskId);
+        // 实际上架位置和分配位置不一致
+        if (!locationId.equals(taskHead.getAllocLocationId())) {
+            BaseinfoLocation realLocation = locationService.getLocation(locationId);
+            if (realLocation == null) {
+                throw new BizCheckedException("2030005");
+            }
+            if (realLocation.getInUse().equals(1)) {
+                throw new BizCheckedException("2030006");
+            }
+        }
+        taskService.done(taskId, locationId);
     }
 
     protected void getConcrete(TaskEntry taskEntry) {
