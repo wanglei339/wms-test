5cb5f925d97c1f74d746fd0950bc27f87f96a8d1
马启迪 <weiandedidi@163.com>
2016-08-17 15:09:04 +0800

    增加container的配置参数containerConstant的笼车和周转箱参数配置,参考阿里巴巴的网站参数

--- wms-core/src/main/java/com/lsh/wms/core/constant/ContainerConstant.java
+++ wms-core/src/main/java/com/lsh/wms/core/constant/ContainerConstant.java
@@ -15,16 +15,34 @@ public class ContainerConstant {
             put(1L, new HashMap<String, Object>() { // 托盘
                 {
                     put("typeName", "托盘");
+                    put("type",1L);
+                    put("model","1.2*1.1*0.1m");
+                    put("capacity",1000L);
+                    put("loadCapacity",2000L);
+                    put("description","木托盘1000L容积,称重2t");
+                    put("status","1");
                 }
             });
             put(2L,new HashMap<String, Object>(){   //笼车
                 {
                     put("typeName","笼车");
+                    put("type",2L);
+                    put("model","1100*800*1700cm");
+                    put("capacity",1000L);
+                    put("loadCapacity",500L);
+                    put("description","笼车1000L容积,称重500KG");
+                    put("status","1");
                 }
             });
             put(3L,new HashMap<String, Object>(){   //周转箱
                 {
                     put("typeName","周转箱");
+                    put("type",2L);
+                    put("model","555*365*295mm");
+                    put("capacity",60L);
+                    put("loadCapacity",30L);
+                    put("description","周转箱60L容积,称重30KG");
+                    put("status","1");
                 }
             });
         }
