2c6472cb3cc419213b687a326c74c8781747f128
lixin <lixin@lsh123.com>
2016-08-23 14:38:12 +0800

    update

--- wms-rpc/src/main/java/com/lsh/wms/rpc/service/system/RolePermissionRestService.java
+++ wms-rpc/src/main/java/com/lsh/wms/rpc/service/system/RolePermissionRestService.java
@@ -19,7 +19,7 @@ import java.util.Map;
  */
 
 @Service(protocol = "rest")
-@Path("sys")
+@Path("role")
 @Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
 @Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
 public class RolePermissionRestService implements IRolePermissionRestService {
