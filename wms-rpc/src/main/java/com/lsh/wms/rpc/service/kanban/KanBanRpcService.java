2071063c7df400ebfaf415ff7eecd2847712c786
lixin <lixin@lsh123.com>
2016-08-26 19:26:35 +0800

    update

---
+++ wms-rpc/src/main/java/com/lsh/wms/rpc/service/kanban/KanBanRpcService.java
@@ -0,0 +1,23 @@
+package com.lsh.wms.rpc.service.kanban;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.lsh.wms.api.service.kanban.IKanBanRpcService;
+import com.lsh.wms.core.service.kanban.KanBanService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by lixin-mac on 16/8/26.
+ */
+@Service(protocol = "dubbo")
+public class KanBanRpcService implements IKanBanRpcService {
+
+    @Autowired
+    private KanBanService kanBanService;
+
+    public List<Map<String, Object>> getKanbanCount(Long type) {
+        return kanBanService.getKanBanCount(type);
+    }
+}
