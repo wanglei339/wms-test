6643cd9b4d273e34cbe7f872b7fdbaf497286409
Frank.F <fancal_king@163.com>
2016-08-26 22:20:38 +0800

    add id generator

---
+++ wms-core/src/main/java/com/lsh/wms/core/constant/IdGeneratorContant.java
@@ -0,0 +1,16 @@
+package com.lsh.wms.core.constant;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by fengkun on 16/8/26.
+ */
+public class IdGeneratorContant {
+    public static final Map<String, Integer> PREFIX_CONFIG = new HashMap<String, Integer>(){
+        {
+            put("task", 1);
+        }
+    };
+
+}
