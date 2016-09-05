c7b65aa72dfb18d48d5673142089a69c11c019bc
马启迪 <weiandedidi@163.com>
2016-09-05 17:30:30 +0800

    新增通过库位获取通道的方法

--- wms-core/src/main/java/com/lsh/wms/core/service/location/LocationService.java
+++ wms-core/src/main/java/com/lsh/wms/core/service/location/LocationService.java
@@ -33,8 +33,8 @@ public class LocationService {
     private StockQuantService stockQuantService;
     @Autowired
     private LocationDetailService locationDetailService;
-    @Autowired
-    private LocationRedisService locationRedisService;
+//    @Autowired
+//    private LocationRedisService locationRedisService;
 
 
     /**
