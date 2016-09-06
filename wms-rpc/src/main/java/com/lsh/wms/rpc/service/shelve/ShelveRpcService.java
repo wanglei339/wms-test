9e95ada8ca67265a02a855cb82f898880cd518ea
Frank.F <fancal_king@163.com>
2016-09-06 20:35:53 +0800

    fix shelve bug

--- wms-rpc/src/main/java/com/lsh/wms/rpc/service/shelve/ShelveRpcService.java
+++ wms-rpc/src/main/java/com/lsh/wms/rpc/service/shelve/ShelveRpcService.java
@@ -185,6 +185,9 @@ public class ShelveRpcService implements IShelveRpcService {
         BaseinfoLocation areaLocation = locationService.getFatherByClassification(location.getLocationId());
         // 获取该库区下所有的货架位
         List<BaseinfoLocation> storeLocations = locationService.getChildrenLocationsByType(areaLocation.getLocationId(), locationType);
+        if (storeLocations.size() < 1) {
+            return false;
+        }
         params.put("itemId", quant.getItemId());
         params.put("locationList", storeLocations);
         // 获取货架位上的该商品库存
