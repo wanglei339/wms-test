d8b82fa9c592ebc0a280518a8cd8c26701836fc6
fuhao <fuhao@lsh123.com>
2016-09-07 22:08:04 +0800

    库存同步

--- wms-integration/src/main/java/com/lsh/wms/integration/service/inventory/SynInventory.java
+++ wms-integration/src/main/java/com/lsh/wms/integration/service/inventory/SynInventory.java
@@ -1,9 +1,21 @@
 package com.lsh.wms.integration.service.inventory;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.config.annotation.Service;
+import com.lsh.atp.api.model.baseVo.ItemDc;
+import com.lsh.atp.api.model.inventory.InventorySyncLshRequest;
+import com.lsh.atp.api.service.inventory.IInventorySyncLshRpcService;
+import com.lsh.base.common.config.PropertyUtils;
 import com.lsh.wms.api.service.inventory.ISynInventory;
+import com.lsh.wms.core.service.item.ItemService;
+import com.lsh.wms.model.baseinfo.BaseinfoItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Project Name: lsh-wms
@@ -18,16 +30,28 @@ import org.slf4j.LoggerFactory;
 @Service(protocol = "dubbo",async=true)
 public class SynInventory implements ISynInventory {
     private static Logger logger = LoggerFactory.getLogger(SynInventory.class);
-    public void synInventory(Long sku_id) {
-        for(int i=0;i<10000;i++){ // TODO: 16/8/20  
-            logger.info(sku_id.toString());
-            try{
-                Thread.currentThread().sleep(1000);
-            }catch (Exception ex){
-                ex.printStackTrace();
-            }
 
-        }
+    @Reference
+    private IInventorySyncLshRpcService iInventorySyncLshRpcService;
 
+
+    @Autowired
+    private ItemService itemService;
+
+
+    public void synInventory(Long item_id,Double qty) {
+        InventorySyncLshRequest request = new InventorySyncLshRequest();
+        request.setZoneCode(PropertyUtils.getString("zone_code"));
+        request.setChannel(PropertyUtils.getInt("channel"));
+        request.setSystem(PropertyUtils.getString("system"));
+        BaseinfoItem  baseinfoItem   = itemService.getItem(item_id);
+        Long ownerId = baseinfoItem.getOwnerId();
+        request.setDcCode(PropertyUtils.getString("owner_"+ownerId));   // TODO: 16/9/7
+        List<ItemDc> skuList = new ArrayList<ItemDc>();
+        ItemDc itemDc = new ItemDc();
+        itemDc.setItemId(item_id);
+        itemDc.setQty(qty.longValue()); // TODO: 16/9/7
+        request.setSkuList(skuList);
+        iInventorySyncLshRpcService.inventorySyncLsh(request);
     }
 }
