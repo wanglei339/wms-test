b2a24761f6eb1f305ee15d7108477156bff93a33
lixin <lixin@lsh123.com>
2016-09-08 10:39:29 +0800

    update

---
+++ wms-core/src/main/java/com/lsh/wms/core/constant/IntegrationConstan.java
@@ -0,0 +1,16 @@
+package com.lsh.wms.core.constant;
+
+/**
+ * Created by lixin-mac on 16/9/7.
+ */
+public class IntegrationConstan {
+
+    /**551报损552报溢*/
+    public static final int LOSS = 551;
+
+    public static final int WIN = 552;
+
+
+    public static final int URL_PO= 10;
+
+}
--- wms-core/src/main/java/com/lsh/wms/core/constant/UrlConstan.java
+++
@@ -1,7 +0,0 @@
-package com.lsh.wms.core.constant;
-
-/**
- * Created by lixin-mac on 16/9/7.
- */
-public class UrlConstan {
-}
