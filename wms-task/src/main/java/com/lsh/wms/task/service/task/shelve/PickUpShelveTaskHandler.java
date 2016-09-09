ae569ec770cf3a99c82889737d83cc58956a983d
wuhao4830 <425677852@qq.com>
2016-09-09 20:21:30 +0800

    update

--- wms-task/src/main/java/com/lsh/wms/task/service/task/shelve/PickUpShelveTaskHandler.java
+++ wms-task/src/main/java/com/lsh/wms/task/service/task/shelve/PickUpShelveTaskHandler.java
@@ -13,6 +13,7 @@ import com.lsh.wms.core.service.stock.StockLotService;
 import com.lsh.wms.core.service.stock.StockQuantService;
 import com.lsh.wms.core.service.task.BaseTaskService;
 import com.lsh.wms.model.stock.StockQuant;
+import com.lsh.wms.model.stock.StockQuantCondition;
 import com.lsh.wms.model.task.TaskEntry;
 import com.lsh.wms.model.task.TaskInfo;
 import com.lsh.wms.task.service.handler.AbsTaskHandler;
@@ -77,6 +78,8 @@ public class PickUpShelveTaskHandler extends AbsTaskHandler {
 
         ObjUtils.bean2bean(quant, taskInfo);
 
+        taskInfo.setQty(stockQuantService.getQuantQtyByContainerId(containerId));
+
         taskInfo.setTaskName("拆零上架任务[ " + taskInfo.getContainerId() + "]");
         taskInfo.setType(TaskConstant.TYPE_PICK_UP_SHELVE);
         taskInfo.setFromLocationId(quant.getLocationId());
@@ -89,6 +92,6 @@ public class PickUpShelveTaskHandler extends AbsTaskHandler {
     public void calcPerformance(TaskInfo taskInfo) {
         taskInfo.setTaskEaQty(taskInfo.getQty());
         taskInfo.setTaskPackQty(taskInfo.getTaskQty().divide(taskInfo.getPackUnit(),2,BigDecimal.ROUND_DOWN));
-        taskInfo.setQtyDone(taskInfo.getQty().divide(taskInfo.getPackUnit(),2, BigDecimal.ROUND_DOWN));
+        taskInfo.setQtyDone(taskInfo.getQty().divide(taskInfo.getPackUnit(), 2, BigDecimal.ROUND_DOWN));
     }
 }
