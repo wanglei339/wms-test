a00bf7ce37154deb4b3cf5d8a9c25d618a6e338a
wuhao4830 <425677852@qq.com>
2016-10-11 20:09:37 +0800

    更新task结构

--- wms-rf/src/main/java/com/lsh/wms/rf/service/inhouse/ProcurementRestService.java
+++ wms-rf/src/main/java/com/lsh/wms/rf/service/inhouse/ProcurementRestService.java
@@ -208,7 +208,6 @@ public class ProcurementRestService implements IProcurementRestService {
     @Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.MULTIPART_FORM_DATA,MediaType.APPLICATION_JSON})
     @Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
     public String fetchTask() throws BizCheckedException {
-        Map<String, Object> params = RequestUtils.getRequest();
         Long uid = 0L;
         try {
             uid =  Long.valueOf(RequestUtils.getHeader("uid"));
