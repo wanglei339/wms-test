9437565239653e1c8c6fa42d56f3502e1b391897
lixin <lixin@lsh123.com>
2016-10-14 14:47:29 +0800

    update

--- wms-core/src/main/java/com/lsh/wms/core/constant/SoConstant.java
+++ wms-core/src/main/java/com/lsh/wms/core/constant/SoConstant.java
@@ -5,8 +5,10 @@ package com.lsh.wms.core.constant;
  */
 public class SoConstant {
 
-    /** 订单类型 1so ,2供商退货  ,3 sto*/
+    /** 订单类型 1so ,2供商退货  ,3 sto ,4直流出库订单*/
     public static final int ORDER_TYPE_SO = 1;
     public static final int ORDER_TYPE_PO_BACK = 2;
     public static final int ORDER_TYPE_STO = 3;
+    public static final int ORDER_TYPE_DIRECT = 4;
+
 }
