461b7bcc119289e4e968289ca5a31c0c22aabde0
马启迪 <weiandedidi@163.com>
2016-10-31 17:33:46 +0800

    PC端操作完成,QC任务完成

--- wms-provider/src/main/java/com/lsh/wms/service/outbound/QCRpcService.java
+++ wms-provider/src/main/java/com/lsh/wms/service/outbound/QCRpcService.java
@@ -1,11 +1,14 @@
 package com.lsh.wms.service.outbound;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.lsh.base.common.exception.BizCheckedException;
 import com.lsh.wms.api.service.pick.IQCRpcService;
+import com.lsh.wms.api.service.task.ITaskRpcService;
 import com.lsh.wms.core.constant.PickConstant;
 import com.lsh.wms.core.constant.WaveConstant;
 import com.lsh.wms.core.service.wave.WaveService;
+import com.lsh.wms.model.task.TaskInfo;
 import com.lsh.wms.model.wave.WaveDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,23 +18,30 @@ import java.math.BigDecimal;
  * Created by zengwenjun on 16/7/30.
  */
 @Service(protocol = "dubbo")
-public class QCRpcService implements IQCRpcService{
+public class QCRpcService implements IQCRpcService {
     @Autowired
     private WaveService waveService;
+    @Reference
+    private ITaskRpcService iTaskRpcService;
 
     public void skipException(long id) throws BizCheckedException {
         WaveDetail detail = waveService.getWaveDetailById(id);
-        if(detail == null){
+        if (detail == null) {
             throw new BizCheckedException("2070001");
         }
-        //必须要进行库存操作
         detail.setQcExceptionDone(PickConstant.QC_EXCEPTION_DONE_SKIP);
+        Long taskId = detail.getQcTaskId();
+        TaskInfo qcTaskInfo = iTaskRpcService.getTaskInfo(taskId);
+        if (null == qcTaskInfo) {
+            throw new BizCheckedException("2070002");
+        }
+        iTaskRpcService.done(taskId, qcTaskInfo.getLocationId());
         waveService.updateDetail(detail);
     }
 
     public void repairException(long id) throws BizCheckedException {
         WaveDetail detail = waveService.getWaveDetailById(id);
-        if(detail == null){
+        if (detail == null) {
             throw new BizCheckedException("2070001");
         }
         detail.setQcExceptionDone(PickConstant.QC_EXCEPTION_DONE_DONE);
@@ -39,11 +49,18 @@ public class QCRpcService implements IQCRpcService{
         detail.setQcExceptionQty(new BigDecimal("0.0000"));
         detail.setQcException(WaveConstant.QC_EXCEPTION_NORMAL);
         waveService.updateDetail(detail);
+
+        Long taskId = detail.getQcTaskId();
+        TaskInfo qcTaskInfo = iTaskRpcService.getTaskInfo(taskId);
+        if (null == qcTaskInfo) {
+            throw new BizCheckedException("2070002");
+        }
+        iTaskRpcService.done(taskId, qcTaskInfo.getLocationId());
     }
 
     public void fallbackException(long id) throws BizCheckedException {
         WaveDetail detail = waveService.getWaveDetailById(id);
-        if(detail == null){
+        if (detail == null) {
             throw new BizCheckedException("2070001");
         }
         detail.setQcExceptionDone(PickConstant.QC_EXCEPTION_DONE_NORMAL);
@@ -51,5 +68,12 @@ public class QCRpcService implements IQCRpcService{
         detail.setQcException(WaveConstant.QC_EXCEPTION_NORMAL);
         detail.setQcExceptionQty(new BigDecimal("0.0000"));
         waveService.updateDetail(detail);
+
+        Long taskId = detail.getQcTaskId();
+        TaskInfo qcTaskInfo = iTaskRpcService.getTaskInfo(taskId);
+        if (null == qcTaskInfo) {
+            throw new BizCheckedException("2070002");
+        }
+        iTaskRpcService.done(taskId, qcTaskInfo.getLocationId());
     }
 }
