9d77ebf6c1bb5b87c3eb01c1195ad48ba536cc29
Frank.F <fancal_king@163.com>
2016-11-02 17:31:25 +0800

    fix bug

--- wms-rf/src/main/java/com/lsh/wms/rf/service/outbound/MergeRfRestService.java
+++ wms-rf/src/main/java/com/lsh/wms/rf/service/outbound/MergeRfRestService.java
@@ -130,7 +130,7 @@ public class MergeRfRestService implements IMergeRfRestService {
                 containerIds.add(containerId);
             }
         }
-        if (containerIds.size() <= 1) {
+        if (containerIds.size() < 1) {
             throw new BizCheckedException("2870005");
         }
         Long mergedContainerId = 0L;
