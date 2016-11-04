46b5c5a11e08a84a7c0ff5a485bdebed8ff4a619
zhanghl0109 <manfeiwu@163.com>
2016-11-04 21:39:20 +0800

    新增大店直流行程单数据接口

---
+++ wms-api/src/main/java/com/lsh/wms/api/service/tu/ITuOrdersRestService.java
@@ -0,0 +1,10 @@
+package com.lsh.wms.api.service.tu;
+
+import com.lsh.base.common.exception.BizCheckedException;
+
+/**
+ * Created by zhanghongling on 16/11/4.
+ */
+public interface ITuOrdersRestService {
+    public String getTuOrdersList(String tuId) throws BizCheckedException;
+}
