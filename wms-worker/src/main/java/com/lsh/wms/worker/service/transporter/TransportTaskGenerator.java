16d79fbf94042b5c03f213aa6c26d13abc985906
justmerry <justmerry@163.com>
2016-11-17 19:20:32 +0800

    transport task generator

---
+++ wms-worker/src/main/java/com/lsh/wms/worker/service/transporter/TransportTaskGenerator.java
@@ -0,0 +1,10 @@
+package com.lsh.wms.worker.service.transporter;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by mali on 16/11/17.
+ */
+@Component
+public class TransportTaskGenerator {
+}
