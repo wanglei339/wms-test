d5fe7b070a75f9678ae777775b7c3a2b8077ae12
justmerry <justmerry@163.com>
2016-11-21 15:23:53 +0800

    add log info for delsoredis

--- wms-core/src/main/java/com/lsh/wms/core/service/inventory/InventoryRedisService.java
+++ wms-core/src/main/java/com/lsh/wms/core/service/inventory/InventoryRedisService.java
@@ -75,7 +75,7 @@ public class InventoryRedisService {
                 continue;
             }
             if (header.getOrderType().equals(SoConstant.ORDER_TYPE_SO) || header.getOrderType().equals(SoConstant.ORDER_TYPE_STO)) {
-                soOrderRedisService.delSoRedis(detail.getOrderId(), detail.getItemId(), detail.getDeliveryQty());
+                soOrderRedisService.delSoRedis(detail.getOrderId(), detail.getItemId(), detail.getQcQty());
                 stockRedisService.outBound(detail.getItemId(), detail.getQcQty());
             }
         }
