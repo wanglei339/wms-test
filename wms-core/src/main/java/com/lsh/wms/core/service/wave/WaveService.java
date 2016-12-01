2233490f56f0aacac3868251a0ba9726b8e0357e
zengwenjun <zengwenjun@lsh123.com>
2016-12-01 14:34:01 +0800

    task info get

--- wms-core/src/main/java/com/lsh/wms/core/service/wave/WaveService.java
+++ wms-core/src/main/java/com/lsh/wms/core/service/wave/WaveService.java
@@ -401,9 +401,6 @@ public class WaveService {
             BigDecimal allocQty = detail.getAllocQty();
             detail.setAllocQty(allocQty.subtract(realSplitQty));
             detail.setAllocUnitQty(PackUtil.EAQty2UomQty(detail.getAllocQty(), detail.getAllocUnitName()));
-            if(detail.getAllocQty().compareTo(BigDecimal.ZERO)<=0){
-                detail.setIsAlive(0L);
-            }
             this.updateDetail(detail);
         }
         return splitDetails;
@@ -490,9 +487,6 @@ public class WaveService {
             this.insertDetail(splitDetail);
             detail.setAllocQty(detail.getPickQty());
             detail.setAllocUnitQty(PackUtil.EAQty2UomQty(detail.getAllocQty(), detail.getAllocUnitName()));
-            if(detail.getAllocQty().compareTo(BigDecimal.ZERO)<=0){
-                detail.setIsAlive(0L);
-            }
             this.updateDetail(detail);
         }
         return splitDetail;
