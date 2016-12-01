056c297258a08f3d7ea5d49bed55f1248d71a389
justmerry <justmerry@163.com>
2016-12-01 19:59:41 +0800

    retrive stock quant mapper

---
+++ wms-task/src/main/java/com/lsh/wms/task/service/TaskRetryService.java
@@ -0,0 +1,11 @@
+package com.lsh.wms.task.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by mali on 16/12/1.
+ */
+@Component
+public class TaskRetryService {
+    
+}
