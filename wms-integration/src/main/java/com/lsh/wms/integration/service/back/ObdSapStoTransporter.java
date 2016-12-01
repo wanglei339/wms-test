fc9f1eca8213b919e3afa25e2494a4fd9116d541
lixin <lixin@lsh123.com>
2016-12-01 22:29:22 +0800

    update

--- wms-integration/src/main/java/com/lsh/wms/integration/service/back/ObdSapStoTransporter.java
+++ wms-integration/src/main/java/com/lsh/wms/integration/service/back/ObdSapStoTransporter.java
@@ -72,6 +72,7 @@ public class ObdSapStoTransporter implements ITransporter{
             //ea转换为包装数量。
             ObdHeader obdHeader = soOrderService.getOutbSoHeaderByOrderId(detail.getOrderId());
             createObdDetail.setDlvQty(PackUtil.EAQty2UomQty(outQty, detail.getPackUnit()).setScale(2,BigDecimal.ROUND_HALF_UP));
+            createObdDetail.setRefDoc(obdHeader.getOrderOtherId());
             createObdDetail.setRefItem(obdDetail.getDetailOtherId());
             createObdDetail.setMaterial(obdDetail.getSkuCode());
             createObdDetail.setOrderType(obdHeader.getOrderType());
