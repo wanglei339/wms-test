d32d142f7f8c068f8e56701878fac06796d52f3d
lixin <lixin@lsh123.com>
2016-12-07 16:19:54 +0800

    update

---
+++ wms-core/src/main/java/com/lsh/wms/core/constant/ReportConstant.java
@@ -0,0 +1,15 @@
+package com.lsh.wms.core.constant;
+
+/**
+ * Created by lixin-mac on 2016/12/7.
+ */
+public class ReportConstant {
+
+    /**来源类型 1 拣货 2 QC 3 补货*/
+    public static final int SOURCE_TYPE_PICK = 1;
+
+    public static final int SOURCE_TYPE_QC = 2;
+
+    public static final int SOURCE_TYPE_PROCUREMENT = 3;
+
+}
