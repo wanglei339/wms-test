1cfb2ea41f2023d18fc8bce44aa162c25bd3d33e
lixin <lixin@lsh123.com>
2016-12-07 17:00:30 +0800

    update

---
+++ wms-api/src/main/java/com/lsh/wms/api/service/datareport/IDifferenceZoneReportRpcService.java
@@ -0,0 +1,14 @@
+package com.lsh.wms.api.service.datareport;
+
+import com.lsh.wms.model.datareport.DifferenceZoneReport;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by lixin-mac on 2016/12/7.
+ */
+public interface IDifferenceZoneReportRpcService {
+    List<DifferenceZoneReport> getDifferenceReportList(Map<String, Object> mapQuery);
+    Integer countDifferenceReport(Map<String, Object> mapQuery);
+}
