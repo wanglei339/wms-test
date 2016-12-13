4ef21a97b126f32b5498df2fb275318448eabdd1
justmerry <justmerry@163.com>
2016-12-13 14:20:33 +0800

    add null area for stock alloc

--- wms-core/src/main/java/com/lsh/wms/core/service/po/PoReceiptService.java
+++ wms-core/src/main/java/com/lsh/wms/core/service/po/PoReceiptService.java
@@ -155,7 +155,7 @@ public class PoReceiptService {
                 // 直流订单在此预占库存
                 StockMove presale = new StockMove();
                 presale.setItemId(move.getItemId());
-                presale.setFromLocationId(locationService.getConsumerArea().getLocationId());
+                presale.setFromLocationId(locationService.getNullArea().getLocationId());
                 presale.setToLocationId(locationService.getSoAreaDirect().getLocationId());
                 presale.setQty(move.getQty());
                 presale.setTaskId(ibdHeader.getOrderId());
