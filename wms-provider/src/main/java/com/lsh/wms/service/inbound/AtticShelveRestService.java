94a6c8525a865c82c5067dad7866757a772cf306
wuhao4830 <425677852@qq.com>
2016-12-14 21:24:41 +0800

    Merge branch 'master' of https://github.com/lianshang/wms
    # Conflicts:
    #	wms-api/src/main/java/com/lsh/wms/api/service/inhouse/IStockTakingRfRestService.java

--- wms-provider/src/main/java/com/lsh/wms/service/inbound/AtticShelveRestService.java
+++ wms-provider/src/main/java/com/lsh/wms/service/inbound/AtticShelveRestService.java
@@ -419,13 +419,13 @@ public class AtticShelveRestService implements IAtticShelveRestService{
         }
 
 
-        Map<String,Object> queryMap = new HashMap<String, Object>();
-        queryMap.put("status",2L);
-        List<TaskEntry> entries = iTaskRpcService.getTaskList(TaskConstant.TYPE_ATTIC_SHELVE, queryMap);
-
-        if(entries!=null && entries.size()!=0) {
-            return JsonUtils.TOKEN_ERROR("改人已存在上架任务");
-        }
+//        Map<String,Object> queryMap = new HashMap<String, Object>();
+//        queryMap.put("status",2L);
+//        List<TaskEntry> entries = iTaskRpcService.getTaskList(TaskConstant.TYPE_ATTIC_SHELVE, queryMap);
+//
+//        if(entries!=null && entries.size()!=0) {
+//            return JsonUtils.TOKEN_ERROR("该人已存在上架任务");
+//        }
 
         TaskEntry entry = iTaskRpcService.getTaskEntryById(taskId);
         if(entry ==null){
