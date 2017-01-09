5d57c11d52d91e00873986b16fa82fa5345736d3
马启迪 <weiandedidi@163.com>
2017-01-10 00:19:45 +0800

    fix已发货的托盘校验修正

--- wms-rf/src/main/java/com/lsh/wms/rf/service/outbound/ShipRestService.java
+++ wms-rf/src/main/java/com/lsh/wms/rf/service/outbound/ShipRestService.java
@@ -163,8 +163,13 @@ public class ShipRestService implements IShipRestService {
                     }
                     //判断合盘的是否被运单运走了
                     List<TuDetail> tuDetails = tuService.getTuDeailListByMergedContainerId(qcInfo.getMergedContainerId());
-                    if (tuDetails != null && tuDetails.size() > 0) {
-                        throw new BizCheckedException("2130014");
+                    if (null != tuDetails && tuDetails.size() > 0) {
+                        for (TuDetail tuDetail : tuDetails) {
+                            TuHead tuHead = tuService.getHeadByTuId(tuDetail.getTuId());
+                            if (!TuConstant.SHIP_OVER.equals(tuHead.getStatus())){
+                                throw new BizCheckedException("2130014");
+                            }
+                        }
                     }
 
                     qcInfos.add(qcInfo);
@@ -346,7 +351,7 @@ public class ShipRestService implements IShipRestService {
         for (String one : transPlanSet) {
             if (count == transPlanSet.size() - 1) {
                 transPlan.append(one);
-            }else {
+            } else {
                 transPlan.append(one);
                 transPlan.append(",");
             }
