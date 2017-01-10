32d26dde86758d66c365105a15e2bf40690f9271
lixin <lixin@lsh123.com>
2017-01-10 15:47:38 +0800

    update

--- wms-rf/src/main/java/com/lsh/wms/rf/service/search/SearchRestService.java
+++ wms-rf/src/main/java/com/lsh/wms/rf/service/search/SearchRestService.java
@@ -147,7 +147,6 @@ public class SearchRestService implements ISearchRestService{
             quantSb.append("箱规 : " + quant.getPackUnit().longValue() + "\n");
             quantSb.append("数量 : " + quant.getQty().setScale(2, BigDecimal.ROUND_HALF_UP) + "\n");
             //quantSb.append("货主 : " + quant.getOwnerId() + " \n ");
-
         }
         Map<String,Object> rep = new HashMap<String, Object>();
         rep.put("data",quantSb);
