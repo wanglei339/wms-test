2d206205761ef8dd568605f4f7ab5337edd2bebe
lixin <lixin@lsh123.com>
2017-01-10 16:50:41 +0800

    update

--- wms-rf/src/main/java/com/lsh/wms/rf/service/search/SearchRestService.java
+++ wms-rf/src/main/java/com/lsh/wms/rf/service/search/SearchRestService.java
@@ -97,7 +97,6 @@ public class SearchRestService implements ISearchRestService{
         }
         //map = new HashMap<String, Object>();
         //匹配正则
-
         boolean result=code.matches("[0-9]+");
         if(result){
             //托盘码
