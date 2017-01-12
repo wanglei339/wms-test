fb39a9859dd8b2cba005b1e8d4d860cdec0a7f29
lixin <lixin@lsh123.com>
2017-01-12 21:58:08 +0800

    看板修改

--- wms-api/src/main/java/com/lsh/wms/api/service/kanban/IKanBanRestService.java
+++ wms-api/src/main/java/com/lsh/wms/api/service/kanban/IKanBanRestService.java
@@ -7,7 +7,7 @@ import java.util.Map;
  */
 public interface IKanBanRestService {
 
-    String getKanbanCount(Long type);
+    String getKanbanCount(Long type,Long subType);
     String getPoKanbanCount(Long type);
     String getPoDetailKanBanCount(Long orderType);
 
--- wms-api/src/main/java/com/lsh/wms/api/service/kanban/IKanBanRpcService.java
+++ wms-api/src/main/java/com/lsh/wms/api/service/kanban/IKanBanRpcService.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * Created by lixin-mac on 16/8/26.
  */
 public interface IKanBanRpcService {
-    List<Map<String, Object>> getKanbanCount(Long type);
+    List<Map<String, Object>> getKanbanCount(Long type,Long subType);
 
     List<Map<String, Object>> getPoKanbanCount(Long type);
 
